2172e270fc38348b7b6a50290318c440c9bd9274
chanhonkeiernest-tech <chanhonkeiernest@gmail.com>
2026-05-20 21:35:11 -0400

    done

---
+++ Unit3- Backend Development/Lesson5- MongoDB-Intro/playground2.mongodb.js
@@ -0,0 +1,11 @@
+use("store");
+
+// db.warehouse.insertOne({ goods:"Milk", price: 6.44 });
+// db.warehouse.insertOne({ goods:"Salmon", price: 12.77 });
+// db.warehouse.insertOne({ goods:"Orange", price: 1.23 });
+
+// db.warehouse.find();
+//db.warehouse.find({ goods: "Salmon" });
+// db.warehouse.update({ goods: "Orange" }, { $set: { price:1.32 } });
+//db.warehouse.deleteOne({ goods: "Milk" })
+db.warehouse.find();
